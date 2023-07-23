2191fb3224605ddebe1d7e59755663255f865373
umermaher <umerrasheedmahar@gmail.com>
2023-07-23 20:29:02 +0500

    implemented chat screen ui showing demo messages based on chat logic

---
+++ lib/utils/String.dart
@@ -0,0 +1,7 @@
+
+class Strings {
+  static const String message = "Message";
+  static const String today = "Today";
+  static const String selectUser = "Select User!";
+  static const empty = "";
+}
